f3fcfe5885f77dba6598960543b5a9b311b99951
renuraj18 <renuraj18@gmail.com>
2017-11-04 17:20:35 +0000

    reload script updated with components

--- resources/scripts/reload_release_notes.sh
+++ resources/scripts/reload_release_notes.sh
@@ -4,7 +4,8 @@
 # available in the usr/share/nginx/html/bp folder.
 
 # Set up initial variables
-release_note=/usr/share/nginx/html
+#release_note=/usr/share/nginx/html
+release_note=/adop-nginx/resources/release_note
 release_note_folder=${release_note}/bp
 n=0;
 
@@ -17,4 +18,15 @@ do
   cat ${release_note}/plugins.json | jq ".blueprints[$n] = {\"url\": \"bp/$this_file\"}" > /tmp/plugins.json.new
   mv /tmp/plugins.json.new ${release_note}/plugins.json
   n=$((n+1))
+done
+n=0
+for f in ${release_note_folder}/*-component.json
+do
+	cat ${release_note}/plugins.json| jq '.core[0].components'| jq "contains(`cat $f|jq '.components'`)"
+        if [ $? -eq 0 ]; then
+                echo "adding"
+                cat ${release_note}/plugins.json | jq ".core[0].components += `cat $f|jq '.components'`" > $release_note/plugins.json.new
+                mv $release_note/plugins.json.new ${release_note}/plugins.json
+                n=$((n+1))
+        fi
 done
